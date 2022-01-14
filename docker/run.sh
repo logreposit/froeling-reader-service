53940b760cb608ad812d7caaa362639426dc96ee
Dominic Miglar <dominic.miglar@netunix.at>
2022-01-14 21:59:59 +0100

    run.sh: try to reduce mem footprint with different, simple GC.

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting application ..."
-java -Xmx128m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/froeling-reader-service/app.jar
+java -Xmx128m -XX:+UseSerialGC -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/froeling-reader-service/app.jar
