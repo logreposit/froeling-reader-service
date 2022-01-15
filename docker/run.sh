561b25c99ad9badb877e607d9399552d5f0ab0dc
Dominic Miglar <dominic.miglar@netunix.at>
2022-01-15 01:00:43 +0100

    docker/run.sh: try maxRam option

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting application ..."
-java -Xmx128m -XX:+UseSerialGC -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/froeling-reader-service/app.jar
+java -XX:MaxRAM=128m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/froeling-reader-service/app.jar
