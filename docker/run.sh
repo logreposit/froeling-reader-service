93cd0846640ade9663d2d7ac62cafdcfaf1cab19
Dominic Miglar <dominic.miglar@netunix.at>
2022-01-15 14:52:17 +0100

    run.sh: removed memory setting again, just take it from the docker container limits

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting application ..."
-java -Xmx64m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/froeling-reader-service/app.jar
+java -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/froeling-reader-service/app.jar
