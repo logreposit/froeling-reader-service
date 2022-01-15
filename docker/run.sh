af6ccc2aa220d728d8be0ab113e041c5196925c9
Dominic Miglar <dominic.miglar@netunix.at>
2022-01-15 13:49:55 +0100

    run.sh: xmx 64m

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting application ..."
-java -Xmx 50m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/froeling-reader-service/app.jar
+java -Xmx64m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/froeling-reader-service/app.jar
