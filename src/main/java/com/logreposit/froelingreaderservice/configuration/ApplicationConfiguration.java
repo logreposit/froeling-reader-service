b4501399b8b5cea300d744b4face91f40f346a77
Dominic Miglar <dominic.miglar@netunix.at>
2022-05-10 10:12:39 +0200

    change to v2 ingress, remove legacy ingress code

--- src/main/java/com/logreposit/froelingreaderservice/configuration/ApplicationConfiguration.java
+++ src/main/java/com/logreposit/froelingreaderservice/configuration/ApplicationConfiguration.java
@@ -1,5 +1,7 @@
 package com.logreposit.froelingreaderservice.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
@@ -7,6 +9,8 @@ import org.springframework.validation.annotation.Validated;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Getter
+@Setter
 @Validated
 @Configuration
 @ConfigurationProperties(value = "froelingreaderservice")
@@ -19,87 +23,5 @@ public class ApplicationConfiguration
     private Long collectInterval;
 
     @NotBlank
-    private String deviceTimezone;
-
-    @NotBlank
     private String apiBaseUrl;
-
-    @NotNull
-    private Integer apiClientRetryCount;
-
-    @NotNull
-    private Long apiClientRetryInitialBackOffInterval;
-
-    @NotNull
-    private Double apiClientRetryBackOffMultiplier;
-
-    public String getDeviceToken()
-    {
-        return this.deviceToken;
-    }
-
-    public void setDeviceToken(String deviceToken)
-    {
-        this.deviceToken = deviceToken;
-    }
-
-    public Long getCollectInterval()
-    {
-        return this.collectInterval;
-    }
-
-    public void setCollectInterval(Long collectInterval)
-    {
-        this.collectInterval = collectInterval;
-    }
-
-    public String getDeviceTimezone()
-    {
-        return this.deviceTimezone;
-    }
-
-    public void setDeviceTimezone(String deviceTimezone)
-    {
-        this.deviceTimezone = deviceTimezone;
-    }
-
-    public String getApiBaseUrl()
-    {
-        return this.apiBaseUrl;
-    }
-
-    public void setApiBaseUrl(String apiBaseUrl)
-    {
-        this.apiBaseUrl = apiBaseUrl;
-    }
-
-    public Integer getApiClientRetryCount()
-    {
-        return this.apiClientRetryCount;
-    }
-
-    public void setApiClientRetryCount(Integer apiClientRetryCount)
-    {
-        this.apiClientRetryCount = apiClientRetryCount;
-    }
-
-    public Long getApiClientRetryInitialBackOffInterval()
-    {
-        return this.apiClientRetryInitialBackOffInterval;
-    }
-
-    public void setApiClientRetryInitialBackOffInterval(Long apiClientRetryInitialBackOffInterval)
-    {
-        this.apiClientRetryInitialBackOffInterval = apiClientRetryInitialBackOffInterval;
-    }
-
-    public Double getApiClientRetryBackOffMultiplier()
-    {
-        return this.apiClientRetryBackOffMultiplier;
-    }
-
-    public void setApiClientRetryBackOffMultiplier(Double apiClientRetryBackOffMultiplier)
-    {
-        this.apiClientRetryBackOffMultiplier = apiClientRetryBackOffMultiplier;
-    }
 }
--- src/main/java/com/logreposit/froelingreaderservice/services/commandexecutor/CommandResult.java
+++ src/main/java/com/logreposit/froelingreaderservice/services/commandexecutor/CommandResult.java
@@ -1,30 +1,3 @@
 package com.logreposit.froelingreaderservice.services.commandexecutor;
 
-public class CommandResult
-{
-    private final int    exitStatus;
-    private final String stdout;
-    private final String stderr;
-
-    public CommandResult(int exitStatus, String stdout, String stderr)
-    {
-        this.exitStatus = exitStatus;
-        this.stdout     = stdout;
-        this.stderr     = stderr;
-    }
-
-    public int getExitStatus()
-    {
-        return this.exitStatus;
-    }
-
-    public String getStdout()
-    {
-        return this.stdout;
-    }
-
-    public String getStderr()
-    {
-        return this.stderr;
-    }
-}
+public record CommandResult(int exitStatus, String stdout, String stderr) {}
